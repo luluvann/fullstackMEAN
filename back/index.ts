b52af8529331acbfa3042d882b3f16d247c2217a
lucile <developer.lvt@gmail.com>
2023-10-20 12:49:41 +0200

    added back server with express

---
+++ back/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+const port = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
